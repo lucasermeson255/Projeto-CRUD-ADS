92d83cbe1067be472d2eef2063a5a92f2e157f9c
Ermeson Lucas <lucasermeson0@gmail.com>
2019-11-05 20:56:56 -0200

    GRUD Inicial

---
+++ ProjetoCRUD/src/univs/edu/usuario/Usuario.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package univs.edu.usuario;
+
+/**
+ *
+ * @author LABORATORIO 01
+ */
+public class Usuario {
+    
+    
+    private int idUsuario;
+    private String nomeUsuario;
+    private String logim;
+    private String  senha;
+
+    /**
+     * @return the idUsuario
+     */
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    /**
+     * @param idUsuario the idUsuario to set
+     */
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    /**
+     * @return the nomeUsuario
+     */
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    /**
+     * @param nomeUsuario the nomeUsuario to set
+     */
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    /**
+     * @return the logim
+     */
+    public String getLogim() {
+        return logim;
+    }
+
+    /**
+     * @param logim the logim to set
+     */
+    public void setLogim(String logim) {
+        this.logim = logim;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+    
+    
+            
+    
+}
