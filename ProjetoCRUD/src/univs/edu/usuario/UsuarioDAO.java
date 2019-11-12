ae4da0a8e232c5f1d421adf8d5015e3bfd5e5e1b
Ermeson Lucas <lucasermeson0@gmail.com>
2019-11-12 21:03:00 -0200

    Fim Método Usuário

--- ProjetoCRUD/src/univs/edu/usuario/UsuarioDAO.java
+++ ProjetoCRUD/src/univs/edu/usuario/UsuarioDAO.java
@@ -5,8 +5,10 @@
  */
 package univs.edu.usuario;
 
+import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
 import univs.edu.util.HibernateUtil;
 
 /**
@@ -14,21 +16,53 @@ import univs.edu.util.HibernateUtil;
  * @author LABORATORIO 01
  */
 public class UsuarioDAO {
-    
-    
+
     private Session sessao;
-   private Transaction trasacao;
-   
-   
-   public void salvar(Usuario usuario){
-     sessao = HibernateUtil.getSessionFactory().openSession();
-     trasacao = sessao.beginTransaction();
-     sessao.save(usuario);
-     trasacao.commit();
-     sessao.close();
-     
-       
-       
-   }
-    
+    private Transaction trasacao;
+
+    public void salvar(Usuario usuario) {
+        sessao = HibernateUtil.getSessionFactory().openSession();
+        trasacao = sessao.beginTransaction();
+        if (usuario.getIdUsuario() == 0) {
+            sessao.save(usuario);
+
+        } else {
+            sessao.update(usuario);
+
+        }
+        trasacao.commit();
+        sessao.close();
+
+    }
+
+    public void excluir(Usuario usuario) {
+
+        sessao = HibernateUtil.getSessionFactory().openSession();
+        trasacao = sessao.beginTransaction();
+        sessao.delete(usuario);
+        trasacao.commit();
+        sessao.close();
+
+    }
+
+    public Usuario pesquisar(int id) {
+
+        sessao = HibernateUtil.getSessionFactory().openSession();
+        trasacao = sessao.beginTransaction();
+        Usuario usuario = (Usuario) sessao.createCriteria(Usuario.class).add(Restrictions.eq("idUsuario", id)).uniqueResult();
+
+        return usuario;
+
+    }
+
+    public List<Usuario> pesquisar() {
+
+        sessao = HibernateUtil.getSessionFactory().openSession();
+        trasacao = sessao.beginTransaction();
+        List<Usuario> usuarios = sessao.createCriteria(Usuario.class).list();
+
+        return usuarios;
+
+    }
+
 }
